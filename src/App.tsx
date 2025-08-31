d117736f04f0a82bcc420ca40a6f1237b9ef3b05
Ohanyere Francis <mooref068@gmail.com>
2025-08-31 07:20:50 -0700

    fix (home) add state in class component

--- src/App.tsx
+++ src/App.tsx
@@ -4,3 +4,13 @@ import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
