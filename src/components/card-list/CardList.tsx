700bc5da5669bdc982322ce6e0c47acac58c5339
Ohanyere Francis <mooref068@gmail.com>
2025-09-01 04:02:47 -0700

    fix(cardList): fix cardList component

--- src/components/card-list/CardList.tsx
+++ src/components/card-list/CardList.tsx
@@ -1,5 +1,15 @@
-const CardList = () => {
-    return (  );
-}
- 
-export default CardList;
+
+
+import  Card  from '../card/Card';
+
+import './card-list.styles.css';
+
+ const CardList = props => (
+  <div className='card-list'>
+    {props.monsters.map(monster => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
+
+export default CardList
